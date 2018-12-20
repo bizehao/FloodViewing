d34af0759552f2e965803368e374c589af7c5661
bzh <2422148686@qq.com>
2018-12-20 13:33:16 +0800

    完善

--- app/src/main/java/com/bzh/floodview/module/home/homeMap/subFram/SubRsvrMapFragment.java
+++ app/src/main/java/com/bzh/floodview/module/home/homeMap/subFram/SubRsvrMapFragment.java
@@ -84,7 +84,6 @@ public class SubRsvrMapFragment extends BaseFragment {
             public void onClick(View v) {
                 Class<?> searchViewClass = mSearchView.getClass();
                 try {
-                    String mm = "你好1111";
                     Method method = searchViewClass.getDeclaredMethod("onSearchClicked");
                     method.setAccessible(true);
                     method.invoke(mSearchView);
